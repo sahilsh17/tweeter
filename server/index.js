542f8d30afddd60b75c6d79942f9bbcdb958c0c7
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-18 05:34:55 +0000

    new article is added for tweets and nav bar is fixed when scrolling down

--- server/index.js
+++ server/index.js
@@ -32,6 +32,12 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
+// //endpoint to POST /tweets/
+// app.post("/tweets", (req, res) => { 
+//   const text = req.body;
+
+//   res.send("Text");
+// })
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
