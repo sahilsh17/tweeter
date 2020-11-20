b8a102ec3c8f98e00dd3525978c70cdab7bf8d2f
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-20 06:26:55 +0000

    no changes

--- server/index.js
+++ server/index.js
@@ -32,12 +32,7 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
-// //endpoint to POST /tweets/
-// app.post("/tweets", (req, res) => { 
-//   const text = req.body;
 
-//   res.send("Text");
-// })
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
